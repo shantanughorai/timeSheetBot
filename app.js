dfa5d1a99b5c162bc4ae71659bb942859cef3445
shantanughorai <shantanu.ghorai@gmail.com>
2017-09-22 00:00:30 +0530

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var Application = require("./lib/app");
+var Server      = require("./lib/server");
+var sdk         = require("./lib/sdk");
+var config      = require("./config");
+
+var app    = new Application(null, config);
+var server = new Server(config, app);
+
+server.start();
+
+sdk.registerBot(require('./FindAFlight.js'));
+sdk.registerBot(require('./SimpleConversationalBot.js'));
+sdk.registerBot(require('./GuessTheNumber.js'));
+sdk.registerBot(require('./BookACab.js'));
+sdk.registerBot(require('./OrderAPizza.js'));
